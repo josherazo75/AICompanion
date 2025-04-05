70ba0bd0a8d85d8c72d5724f8f7b2b607377bf23
josherazo75 <erazojoshua68@gmail.com>
2025-04-04 23:47:38 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  root: 'client',
+  root: "client",
   plugins: [react()],
   build: {
-    outDir: '../dist/public', // static files will end up in dist/public
+    outDir: "../dist/client",
     emptyOutDir: true,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'client'),
+      "@": path.resolve(__dirname, "./client/src"),
     },
   },
 });
